f41a94735eb7a10cb88c66ba481ce6dd9b1a09f5
automationchad <will.marzella@tray.io>
2022-10-06 15:18:50 -0700

    update head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
     build: {
     transpile: ['@heroicons/vue', '@headlessui/vue']
+  },
+  head: {
+    title: 'Tray Pricing',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+      // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+      { hid: 'description', name: 'description', content: 'Meta description' }
+    ]
   }
 })
